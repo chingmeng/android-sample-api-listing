7be57b2dda8b242d8b33735ac32195cd3b532a4f
chingmeng <sum2infinity@gmail.com>
2020-11-03 16:31:00 +0800

    Add basic files and configuration
    - Main Activity

---
+++ app/src/main/java/com/android/tutorial/sample_api_listing/view/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.android.tutorial.sample_api_listing.view
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.android.tutorial.sample_api_listing.R
+import com.android.tutorial.sample_api_listing.util.init
+
+class MainActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
