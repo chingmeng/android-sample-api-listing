1238f677e8d3ea1b31885c6e3f280cabdbe5f380
chingmeng <sum2infinity@gmail.com>
2020-11-03 16:39:34 +0800

    Setup MainActivity to navigate to SampleListingFragment

--- app/src/main/java/com/android/tutorial/sample_api_listing/view/MainActivity.kt
+++ app/src/main/java/com/android/tutorial/sample_api_listing/view/MainActivity.kt
@@ -10,5 +10,8 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        SampleListingFragment().init(this)
+
     }
 }
